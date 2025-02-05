a37b937ffffbe8f9980442d772cbc287e7ab2ae8
Nicola TEB <nicosia.k@interia.pl>
2025-02-05 11:10:43 +0100

    Book class

---
+++ BibliotekaKsiazek/MVVM/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaKsiazek.MVVM.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public double Rate { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
